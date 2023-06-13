54e0dfd38de532dfe8c53dd245118275d37e7a56
Eric Mugo <mugoericpg20@gmail.com>
2023-06-13 13:27:24 +0300

    dev

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,15 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+  
+    def received_one?(item_name)
+      freebies.exists?(item_name: item_name)
+    end
+  
+    def give_away(dev, freebie)
+      return unless freebies.include?(freebie)
+      freebie.update(dev: dev)
+    end
+
 
 end
